2a495a86b181ada7b2496f1dee953908daebd0da
carolina.marin <carolina.marin@landsoft.com.co>
2020-04-27 00:53:09 -0500

    Creación pantallas de pasajeros y vuelos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+
+import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
+import { NuevoPasajeroComponent } from './components/nuevo-pasajero/nuevo-pasajero.component';
+import { NuevoVueloComponent } from './components/nuevo-vuelo/nuevo-vuelo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoPasajeroComponent,
+    NuevoVueloComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
